4f524fe9be44d49e90b75910f3c94b0bc22523da
Muhammad Zahid <muhammadali775@gmail.com>
2021-07-06 03:19:29 -0400

    commit tree 694394b090fe82e55dfe171120ad971dec361f16

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,17 @@
 class ItemsController < ApplicationController
 
   def index
-    items = Item.all
-    render json: items, include: :user
-  end
 
+    if params[:user_id]
+        user= User.find(params[:user_id])
+        items = user.items
+    else
+       
+      items = Item.all
+   
+    end
+   
+     render json: items, include: :user
+
+  end
 end
